881891db7b8615cd709805f4a595bef228bc0a08
yira <dladlfk1@gmail.com>
2023-06-17 19:00:41 +0900

    google login

--- src/pages/login/google/Token.ts
+++ src/pages/login/google/Token.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 export const requestToken = async (token: string) => {
   const res: AxiosResponse = await axios.post(
-    "http://localhost:8080/api/oauth/google",
+    "https://resume-api.com/api/oauth/google",
     {
       token,
     }
