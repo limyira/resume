d28975f341d0caa081a612880b7a73ddb53e1917
yira <dladlfk1@gmail.com>
2023-06-17 19:27:34 +0900

    google login error

--- src/pages/login/google/Token.ts
+++ src/pages/login/google/Token.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 export const requestToken = async (token: string) => {
+  console.log(token);
   const res: AxiosResponse = await axios.post(
     "https://resume-api.com/api/oauth/google",
     {
